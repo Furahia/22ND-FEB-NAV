8309f30da42092268925941f31ce911dcbe60004
Bonny CS Admin NETWORKS <brendahjoy5@gmail.com>
2023-02-22 08:43:20 +0300

    add items

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Header() {
+    return(
+        <div className="header">THIS IS THE HEADER</div>
+    )
+    
+}
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Navbar() {
+    return(
+        <div className="Nav">
+            <ul>
+
+<li> 00 TIME</li>    
+<li>01 DESTINATION</li>
+<li> 02 CREW</li>
+<li>03 TECHNOLOGY </li>
+</ul>
+    </div>
+    )
+    
+}
+export default Navbar
